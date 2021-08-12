f6b4933edca9ecd0e1ea907100546418db5b3d72
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-12 14:10:10 -0500

    Exercise Completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Jaime";
+const lName = "Orlando";
+const luckyNumber = 11;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, {fName + " " + lName}!</h1>
+    <p> Your Lucky number is {Math.floor(Math.random() * 10) + 1}</p>
+  </div>,
+  document.getElementById("root")
+);
